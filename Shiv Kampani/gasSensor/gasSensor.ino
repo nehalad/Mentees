7e6889a5f64967e9d4ef67a2d4e0b240c03728f7
siddheshmurudkar <32839650+siddheshmurudkar@users.noreply.github.com>
2018-01-05 14:22:13 +0530

    Update gasSensor.ino

--- Shiv Kampani/gasSensor/gasSensor.ino
+++ Shiv Kampani/gasSensor/gasSensor.ino
@@ -3,10 +3,20 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 #include <EEPROM.h>
+#include <NeoPixelBus.h>
 
-String IFTTT_KEY = ""; //change this with your ifttt key
-const char* eventName  = "gas_sensor";   
+const uint16_t PixelCount = 1; // this example assumes 4 pixels, making it smaller will cause a failure
+const uint8_t PixelPin = 4;  // make sure to set this to the correct pin, ignored for Esp8266
+#define colorSaturation 128
+NeoPixelBus<NeoGrbFeature, NeoEsp8266BitBang800KbpsMethod> strip(PixelCount, PixelPin);
+RgbColor red(colorSaturation, 0, 0);
+RgbColor black(0);
+
+String IFTTT_KEY = "do7N3ibZVo254_ibYIYrY4"; 
+// my api key  iYiYhj3KyPFEwyVRuJzEb(for testing)
+const char* eventName  = "gas_sensor";
 const char* IFTTT_URL = "maker.ifttt.com";
+
 
 #define SLEEP_TIME 180*1*1000000     //change sleep time, 180 seconds for 3 minutes
 #define WIFI_CONNECT_TIMEOUT_S 20                   // max time for wifi connect to router, if exceeded restart
@@ -23,6 +33,9 @@ void setup()
       
       Serial.begin(115200);
       Serial.println();
+      // this resets all the neopixels to an off state
+      strip.Begin();
+      strip.Show();
       pinMode(sensorPin, INPUT);
       pinMode(buzzerPin, OUTPUT);
       digitalWrite(buzzerPin, LOW);
@@ -67,7 +80,12 @@ void setup()
                // Set some values for the JSON data depending on which event has been triggered
                IPAddress ip = WiFi.localIP();
                String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-  
+               // String value_1 = String(t);
+               //String value_2 = String(h);
+               //String value_3 = "";
+       
+
+
                // Build JSON data string
                String data = "";
                // data = data + "\n" + "{\"value1\":\"" + value_1 + "\"}"; //,"value2\":\""+ value_2 +"\"}";
@@ -104,9 +122,13 @@ void setup()
                       {
                         //  Serial.println("Buzzer ON"); 
                         digitalWrite(buzzerPin, HIGH);
+                        strip.SetPixelColor(0, red);
+                        strip.Show();
                         delay(500);
                         //  Serial.println("Buzzer OFF");
                         digitalWrite(buzzerPin, LOW);
+                        strip.SetPixelColor(0, black);
+                        strip.Show();
                         delay(500);
                       }
             
@@ -125,6 +147,10 @@ void loop() {
 
 
   }
+
+  
+
+
 
   
 
