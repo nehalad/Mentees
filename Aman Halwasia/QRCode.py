bae977ac02b09848fb6b4749106a889d721ffc66
Rupin <rupin@curiositygym.com>
2018-04-16 12:34:29 +0000

    test program

---
+++ Aman Halwasia/QRCode.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from PIL import Image
+from pyzbar.pyzbar import decode
+class Application():
+
+    def detect_red_circle(self):
+        cam = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cam.read()
+	    print(frame)
+            impil=Image.fromarray(frame)
+            a=decode(impil)
+            print(a)
+            
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+if __name__ == '__main__':
+    app =Application()
+    app.detect_red_circle()
